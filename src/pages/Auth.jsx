11693000e9824f59fcd5280b28ad6230bd02ca14
sujikim <sujikim@student.42seoul.kr>
2023-03-15 22:40:44 +0900

    [FEAT] OAuth 프론트 요청코드 추가

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
 const Auth = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const code = searchParams.get("code");
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API}/auth/me`, {
+        params: { code: code },
+        withCredentials: true,
+      })
+      .then(() => {
+        navigate("/total");
+      });
+  }, []);
+
   return <div>Auth</div>;
 };
 
