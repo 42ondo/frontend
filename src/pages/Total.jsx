669c96efde50a423733ae0db1ba6037a696682c1
sujikim <sujikim@student.42seoul.kr>
2023-03-17 16:36:22 +0900

    [CHORE] 테스트 예제 삭제

--- src/pages/Total.jsx
+++ src/pages/Total.jsx
@@ -1,23 +1,7 @@
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { useApi } from "../hooks/useApi";
+import react from "react";
+
 function TotalPage() {
-  const { useGet } = useApi();
-
-  const testCall = () => {
-    useGet("test", "example/test");
-  };
-  const logout = () => {
-    useGet("logout", "auth/logout");
-  };
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <button onClick={testCall}>test</button>
-      <button onClick={logout}>logout</button>
-      <Link to={process.env.REACT_APP_OAUTH_LINK}>Login</Link>
-    </div>
-  );
+  return <>Total</>;
 }
 
 export default TotalPage;
