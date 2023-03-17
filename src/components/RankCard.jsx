6cec4b8e4c244494ee91fcaa3ffc62e9ed83187e
cmoon <chihun0507@naver.com>
2023-03-17 19:17:51 +0900

    feat: :sparkles: RankCard 컴포넌트 제작

---
+++ src/components/RankCard.jsx
@@ -0,0 +1,83 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  VStack,
+  Text,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
+import UserItem from "./UserItem";
+function RankCard() {
+  const users = [
+    {
+      id: 1234,
+      name: "cmoon",
+      imgUrl:
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/피카츄_공식_일러스트.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+      ondo: 42,
+    },
+    {
+      id: 1235,
+      name: "dongyshi",
+      imgUrl:
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/피카츄_공식_일러스트.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+      ondo: 42,
+    },
+    {
+      id: 1236,
+      name: "juahn",
+      imgUrl:
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/피카츄_공식_일러스트.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+      ondo: 42,
+    },
+    {
+      id: 1237,
+      name: "sujikim",
+      imgUrl:
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/피카츄_공식_일러스트.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+      ondo: 42,
+    },
+    {
+      id: 1238,
+      name: "tmp",
+      imgUrl:
+        "https://static.wikia.nocookie.net/pokemon/images/5/52/피카츄_공식_일러스트.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko",
+      ondo: 42,
+    },
+  ];
+  const unit = "도";
+  return (
+    <>
+      <Box p="5" alignItems="center" w="20%" borderRadius="10" bg="#C476D1">
+        <VStack spacing={5}>
+          <Card w="80%">
+            <CardBody>
+              <Text align="center">Rank Card Title</Text>
+            </CardBody>
+          </Card>
+          <Card w="100%">
+            <CardBody>
+              <VStack mt="1" spacing="3">
+                {users.map((x, i) => (
+                  <UserItem idx={i} user={x} cnt={x.ondo} unit={unit} />
+                ))}
+              </VStack>
+            </CardBody>
+          </Card>
+        </VStack>
+      </Box>
+    </>
+  );
+}
+export default RankCard;
+
+/*
+type user = {
+ id: number,
+ name : string,
+ imgUrl : string,
+ ondo : number
+}
+*/
