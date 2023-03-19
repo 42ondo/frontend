feb528578b9eb316a8045c59ea983124a206bd0f
sujikim <sujikim@student.42seoul.kr>
2023-03-20 04:48:43 +0900

    [FEAT] 네트워크 에러시 토스트로 알림

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,18 +1,33 @@
 import useAxios from "./useAxios";
 import { useQuery } from "react-query";
+import { useToast } from "@chakra-ui/react";
 
 export const useApi = () => {
   const { get } = useAxios();
+  const toast = useToast();
 
+  const displayErrorToast = (error) => {
+    toast({
+      title: "예상치 못한 오류가 발생했습니다.",
+      description: error.message,
+      status: "error",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
   /*
   params
     suspense: react-query suspense 설정,
     config: AxiosRequestConfig
   */
-  const useGet = (key, url, data, { onSuccess, onError, config, suspense }) =>
-    useQuery(key, () => get(url, data, config).then(onSuccess).catch(onError), {
-      suspence: suspense,
-    });
+  const useGet = (key, url, data, { onSuccess, config, suspense }) =>
+    useQuery(
+      key,
+      () => get(url, data, config).then(onSuccess).catch(displayErrorToast),
+      {
+        suspence: suspense,
+      }
+    );
 
   return { useGet };
 };
